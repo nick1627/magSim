045aa0033e1e2defcc0aec7827e1f2281302a144
nick1627 <nick.rayns1627@gmail.com>
2021-10-10 19:03:21 +0100

    added limited functionality for 2D plane plotting

--- Nick/fields.py
+++ Nick/fields.py
@@ -39,36 +39,85 @@ class Field:
         #This is a placeholder
         return self.B
 
-    def plotField(self, xmin, xmax, ymin, ymax, zmin, zmax, xstep = 1000, ystep = 1000, zstep = 1000):
+    def plot3DField(self, xmin, xmax, ymin, ymax, zmin, zmax, scale = 1):
+        xstep = (xmax - xmin)/4
+        ystep = (ymax - ymin)/4
+        zstep = (zmax - zmin)/4
+
+
         x = np.arange(xmin, xmax + xstep, xstep)
         y = np.arange(ymin, ymax + ystep, ystep)
         z = np.arange(zmin, zmax + zstep, zstep)
 
         grid_x, grid_y, grid_z = np.meshgrid(x, y, z)
-        u, v, w = np.array(np.shape(grid_x))
+        u = np.zeros(np.shape(grid_x))
+        v = np.zeros(np.shape(grid_x))
+        w = np.zeros(np.shape(grid_x))
 
         for i in range(0, np.shape(grid_x)[0]):
             for j in range(0, np.shape(grid_x)[1]):
                 for k in range(0, np.shape(grid_x)[2]):
                     r = np.array([grid_x[i, j, k], grid_y[i, j, k], grid_z[i, j, k]])
                     B = self.getField(r)
+                    # print(u[i,j,k])
+                    # print(B[0])
                     u[i, j, k] = B[0]
                     v[i, j, k] = B[1]
                     w[i, j, k] = B[2]
 
-
-
-        # for xval in x:
-        #     for yval in y:
-        #         for zval in z:
-        #             r = np.array([xval, yval, zval])
-        #             [u, v, w] = self.getField(r) 
-
-
-        ax = plt.figure().add_subplot(projection = "3d")
-        ax.quiver(x, y, z, u, v, w)
+        u = scale*u
+        v = scale*v
+        w = scale*w
         
 
+        ax = plt.figure().add_subplot(projection = "3d")
+        ax.quiver(grid_x, grid_y, grid_z, u, v, w)
+
+        return
+        
+
+    def plot2DField(self, a, b, N, scale = 1):
+        #  This plots a 2D vector field (arrows may be in 3D) as a plane
+        #  Normal vector of the plane is n, specified by user
+        #  Plane will always intersect the origin
+
+        #a is a vector in the plane
+        #b is another vector in the plane, perpendicular to a
+        #N is the number of vectors a or b that make up the side length of the plane
+
+        x = np.zeros((N + 1, N + 1))
+      
+        y = np.zeros(np.shape(x))
+        z = np.zeros(np.shape(x))
+
+        u = np.zeros(np.shape(x))
+        v = np.zeros(np.shape(x))
+        w = np.zeros(np.shape(x))
+
+        start = np.array([0,0,0]) - (N/2)*a - (N/2)*b
+
+        for i in range(0, np.shape(x)[0]):
+            for j in range(0, np.shape(x)[1]):
+                currentVector = start + i*a + j*b
+
+                x[i, j] = currentVector[0]
+                y[i, j] = currentVector[1]
+                z[i, j] = currentVector[2]
+
+                currentB = self.getField(currentVector)
+                u[i, j] = currentB[0]
+                v[i, j] = currentB[1]
+                w[i, j] = currentB[2]
+
+        u = scale*u
+        v = scale*v
+        w = scale*w
+
+
+        ax2 = plt.figure().add_subplot(projection = "3d")
+        ax2.quiver(x, y, z, u, v, w)
+
+        return
 
 
 
@@ -88,7 +137,7 @@ class SHField(Field):
         self.nMax = 1 #Need to get this from the shape of g etc
  
 
-    def PnmCos(n, m, theta): #Returns Pnm(cos(theta)) for n up to 2
+    def PnmCos(self, n, m, theta): #Returns Pnm(cos(theta)) for n up to 2
         if n == 2:
             if m == 0:
                 return 1.5*(pow(np.cos(theta), 2) - 1/3)
@@ -110,7 +159,7 @@ class SHField(Field):
         else:
             raise Exception("Error:  n = " + n + " is invalid!")
         
-    def PnmCosDerivative(n, m, theta): #Returns derivative of Pnm(cos(theta)) wrt theta for n up to 2
+    def PnmCosDerivative(self, n, m, theta): #Returns derivative of Pnm(cos(theta)) wrt theta for n up to 2
         if n == 2:
             if m == 0:
                 return -3*np.sin(theta)*np.cos(theta)
@@ -159,18 +208,20 @@ class SHField(Field):
             frac2 = frac**(n+2)
             for m in range(0, n+1):
                 #g[n,m] difference in matrix index to n value
-                print(n, m, theta)
-                Br += (n+1)*frac2*(self.g[n-1, m]*np.cos(m*phi) + self.h[n-1,m]*np.sin(m*phi))*self.PnmCos(n, m, theta)
-                Btheta -= (frac2*(self.g[n-1, m]*np.cos(m*phi) + self.h[n-1,m]*np.sin(m*phi))*self.PnmCosDerivative(n, m, theta))
-                Bphi += m*frac2*(self.g[n-1, m]*np.sin(m*phi) - self.h[n-1,m]*np.cos(m*phi))*self.PnmCos(n, m, theta)
+                # print(n, m, theta)
+                # print(np.shape(theta))
+                # print(self.PnmCos(n, m, theta))
+                Br += (n+1)*frac2*(self.g[n-1, m]*np.cos(m*phi) + self.h[n-1, m]*np.sin(m*phi))*self.PnmCos(n, m, theta)
+                Btheta -= (frac2*(self.g[n-1, m]*np.cos(m*phi) + self.h[n-1, m]*np.sin(m*phi))*self.PnmCosDerivative(n, m, theta))
+                Bphi += m*frac2*(self.g[n-1, m]*np.sin(m*phi) - self.h[n-1, m]*np.cos(m*phi))*self.PnmCos(n, m, theta)
         
         Bphi *= 1/np.sin(theta)
 
         #Now need to recover cartesian mag field components
         Bspherical = np.array([Br, Btheta, Bphi])
-        T = np.array([[np.sin(theta)*np.cos(phi), np.cos(theta)*np.cos(phi), -np.sin(theta)], 
-                        [np.sin(theta)*np.sin(phi), np.cos(theta)*np.sin(phi), np.cos(theta)],
-                        [np.cos(theta), -np.sin(theta), 0]])
+        T = np.array([[np.sin(theta)*np.cos(phi),   np.cos(theta)*np.cos(phi),  -np.sin(theta)], 
+                        [np.sin(theta)*np.sin(phi), np.cos(theta)*np.sin(phi),  np.cos(theta)],
+                        [np.cos(theta),             -np.sin(theta),             0]])
 
         Bcartesian = np.matmul(T, Bspherical)
 
--- Nick/test.py
+++ Nick/test.py
@@ -8,11 +8,39 @@ from fields import SHField
 
 import fields as f
 
-g = np.array([[10000, 10000]])
-h = np.array([[0, -10000]])
-field1 = SHField(1000000, g, h, 0, 0)
 
-field1.plotField(-100000, 100000, -100000, 100000, -100000, 100000)
+
+
+#Plotting field of Neptune since it's a bit less weird
+Rn = 24765000 #radius of neptune in metres
+#start with dipole only
+g = np.array([[0.09732, 0.03220]])
+h = np.array([[0, -0.09889]])
+
+field1 = SHField(Rn, g, h, 0, 0)
+
+#field1.plot3DField(-1.5*Rn, 1.5*Rn, -1.5*Rn, 1.5*Rn, -1.5*Rn, 1.5*Rn, scale = 100000000)
+field1.plot2DField(np.array([Rn, 0, 0]), np.array([0, Rn, 0]), 5)
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# ax = plt.figure().add_subplot(projection='3d')
+
+# # Make the grid
+# x, y, z = np.meshgrid(np.arange(-0.5, 1, 0.5),
+#                       np.arange(-0.5, 1, 0.5),
+#                       np.arange(-0.5, 1, 0.5))
+
+# print(np.shape(x))
+
+# # Make the direction data for the arrows
+# u = np.ones(np.shape(x))
+# v = np.ones(np.shape(x))
+# w = np.ones(np.shape(x))
+
+# ax.quiver(x, y, z, u, v, w, length=0.1, normalize=True)
+
 
 
 plt.show()
