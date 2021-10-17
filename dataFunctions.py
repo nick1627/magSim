9ae1050ad4242aa61a0bb8b9132e316d62d91609
nick1627 <nick.rayns1627@gmail.com>
2021-10-17 16:43:55 +0100

    updated quadrupole results, working on saving arrays

--- dataFunctions.py
+++ dataFunctions.py
@@ -1,8 +1,15 @@
 """
 Contains code that saves and loads data so we can compare results etc.
 """
-
+import numpy as np
 
 def saveBField(x, y, z, u, v, w, filePath):
     # This saves the data for plotting a field in the provided path
+    
+    np.savez(filePath, x = x, y= y, z = z, u = u, v = v, w = w)
+    
     return
+
+def loadBField(filePath):
+    x, y, z, u, v, w = np.load(filePath)
+    return x, y, z, u, v, w
